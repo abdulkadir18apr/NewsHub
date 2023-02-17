ae03c5b6e304c77f064dad8575e389c309e19c1e
abdulkadir18apr <114944605+abdulkadir18apr@users.noreply.github.com>
2023-02-17 19:45:21 +0530

    Added infiniteScrolling

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -19,9 +19,8 @@ export class News extends Component {
       }
     
     articles=[] ;
-    static propTypes = {}
-    constructor(){
-    super();
+    constructor(props){
+    super(props);
     this.state={
         articles:this.articles,
         loading:false,
@@ -38,38 +37,32 @@ export class News extends Component {
     let respose=await fetch(url)
     let data=await respose.json()
     this.setState({articles:data.articles,totalArticles:data.totalResults, loading:false});
-    console.log(data)
+
   }
-   HandleNext=async()=>{
-        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=407c5b1395e04accb74e85ffe7589e13&page=${this.state.page+1}&pagesize=${this.props.pageSize}`;
-        this.setState({loading:true});
-        let respose=await fetch(url)
-        let data=await respose.json()
-        this.setState({articles:data.articles,
-                        page:this.state.page+1,
-                    loading:false});
-  }
-  HandlePrev=async()=>{
-    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=407c5b1395e04accb74e85ffe7589e13&page=${this.state.page-1}&pagesize=${this.props.pageSize}`
+  fetchMoreData=async()=>{
+    this.setState({page:this.state.page+1});
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=407c5b1395e04accb74e85ffe7589e13&page=1&pagesize=${this.props.pageSize}`
     this.setState({loading:true});
     let respose=await fetch(url)
     let data=await respose.json()
-    this.setState({articles:data.articles,
-                    page:this.state.page-1,
-                loading:false});
-
+    this.setState({articles:this.state.articles.concat(data.articles),totalArticles:data.totalResults, loading:false});
   }
+  i=0
+  increment=()=>{
+    this.i=this.i+1;
+  }
+
+
+
 
 
   
 
   render() {
-    // let{pageSize}=this.props;
 
     return (
       <div className="container my-3">
         <h2 className='text-center'>NewsHub Top HeadLines</h2>
-        {/* {this.state.loading && <Spinner/>} */}
         <div className="row">
         <InfiniteScroll
           dataLength={this.state.articles.length}
@@ -80,7 +73,7 @@ export class News extends Component {
         >
             {this.state.articles.map((element)=>{
             return(
-                <div className="col-md-4" key={element.url}>
+                <div className="col-md-4" key={this.increment()}>
                 <NewsItem  title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage?element.urlToImage:"https://assets1.cbsnewsstatic.com/hub/i/r/2023/02/13/191b867e-6b6c-4655-ac7b-7d05f7306ee7/thumbnail/1200x630/c53519ebb168e04fba72b027a0e8a50c/gettyimages-1246627710.jpg"} newsUrl={element.url} />                
                 </div>
             )
@@ -91,11 +84,6 @@ export class News extends Component {
 
 
         
-    </div>
-    <div className="d-flex justify-content-between">
-        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.HandlePrev}>&laquo; Previous </button>
-        <button disabled={Math.ceil(this.totalArticles/this.props.pageSize)<=this.state.page} type="button" className="btn btn-dark" onClick={this.HandleNext}>Next &raquo;</button>
-
     </div>
         
       </div>
